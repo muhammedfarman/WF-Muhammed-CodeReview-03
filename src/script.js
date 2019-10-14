ab424355851868fd4f40baa25c1d0074676fd543
Mo Faraman <mofarman102@gmail.com>
2019-10-14 11:25:30 +0200

    likes and sort working fine now

--- src/script.js
+++ src/script.js
@@ -3,97 +3,80 @@ var movies = [];
 movies = movie;
 //input function//
 function output() {
-    let card = document.createElement("div");
-    let info = document.createElement("P");
-    info.setAttribute("class", "title")
-    info.innerHTML = movie[i].title + movie[i].info;
-    card.setAttribute("class", "cards");
-    card.appendChild(info);
-    let element = document.getElementById("container");
-    element.appendChild(card);
-    let x = document.createElement("img");
-    x.setAttribute("class", "poster");
-    x.setAttribute("src", movie[i].img);
-    x.setAttribute("alt", movie[i].title);
-    card.appendChild(x);
-    l = movie[i].likes;
-    let counter = document.createElement("button");
-    counter.setAttribute("class", "counter");
-    counter.setAttribute("id", "clickme" + movie[i].id);
-    counter.setAttribute("value", movie[i].id);
-    counter.innerHTML = "LIKE : " + l;
-    info.appendChild(counter);
-
+    for (var i = 0; i < movies.length; i++) {
+        let card = document.createElement("div");
+        let info = document.createElement("P");
+        info.setAttribute("class", "title")
+        info.innerHTML = movie[i].title + movie[i].info;
+        card.setAttribute("class", "cards");
+        card.appendChild(info);
+        let element = document.getElementById("container");
+        element.appendChild(card);
+        let x = document.createElement("img");
+        x.setAttribute("class", "poster");
+        x.setAttribute("src", movie[i].img);
+        x.setAttribute("alt", movie[i].title);
+        card.appendChild(x);
+        l = movie[i].likes;
+        let counter = document.createElement("button");
+        counter.setAttribute("class", "counter");
+        counter.setAttribute("id", "clickme" + movie[i].id);
+        counter.setAttribute("value", movie[i].id);
+        counter.innerHTML = "LIKE : " + l;
+        info.appendChild(counter);
+    }
 }
 
 //LOAD input from json//
 
-for (var i = 0; i < movie.length; i++) {
-    output(movie);
-}
+
+output(movies);
+//call like function //
+likecounter();
 
 
 // like clicks counter//
-let buttons = document.querySelectorAll('.counter');
-like(buttons);
-function like() {
-    for (let but of buttons) {
-        but.addEventListener('click', (e) => {
-            let x = e.target.attributes[2].value;
-            let ct = e.target.parentNode.children[0];
-            let c = e.target.parentNode.children[0].innerText.split(" ")[2];
-            c++;
-            ct.innerHTML = "LIKE : " + c;
+function likecounter() {
+    let buttons = document.querySelectorAll('.counter');
+    like(buttons);
+    function like() {
+        for (let but of buttons) {
+            but.addEventListener('click', (e) => {
+                let x = e.target.attributes[2].value;
+                let ct = e.target.parentNode.children[0];
+                let c = e.target.parentNode.children[0].innerText.split(" ")[2];
+                c++;
+                ct.innerHTML = "LIKE : " + c;
+                movie[x].likes = c;
 
-        })
-    };
+            })
+        };
+    }
 }
 // end like clicks counter//
 //// sorting button ////
 
+let moviesorted = [];
+moviesorted = movie;
 
 function sorting() {
-    
-    movies.sort(function(a, b) {
+
+
+    moviesorted.sort(function (a, b) {
         return parseFloat(b.likes) - parseFloat(a.likes);
     });
-console.log(movies);
+    console.log(movie);
 
 }
-let sortbutton=document.getElementById("sort1");
-sortbutton.addEventListener('click', ()=>{
+let sortbutton = document.getElementById("sort1");
+sortbutton.addEventListener('click', () => {
+
     removeold();//remove old container stuff
     sorting();//sorting the array
-    sortlist();// outputing the array
-  
+    output(moviesorted);// outputing the array
+    likecounter();
 });
-function sortlist(){
-for (var i=0 ; i < movies.length ; i++){
-    let card = document.createElement("div");
-    let info = document.createElement("P");
-    info.setAttribute("class", "title")
-    info.innerHTML = movie[i].title + movie[i].info;
-    card.setAttribute("class", "cards");
-    card.appendChild(info);
-    let element = document.getElementById("container");
-    element.appendChild(card);
-    let x = document.createElement("img");
-    x.setAttribute("class", "poster");
-    x.setAttribute("src", movie[i].img);
-    x.setAttribute("alt", movie[i].title);
-    card.appendChild(x);
-    l = movie[i].likes;
-    let counter = document.createElement("button");
-    counter.setAttribute("class", "counter");
-    counter.setAttribute("id", "clickme" + movie[i].id);
-    counter.setAttribute("value", movie[i].id);
-    counter.innerHTML = "LIKE : " + l;
-    info.appendChild(counter);
 
-   
-   
-}}
-function removeold(container)
-{
+function removeold(container) {
     document.getElementById("container").innerHTML = "";
 }
